126a515bbb3505bbb33a090c9a543e3449a78a75
Michael Thiessen <electromikenetic@gmail.com>
2015-12-15 00:09:08 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+
+gulp.task('postcss', ['sass'], function() {
+    var postcss      = require('gulp-postcss');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('./assets/css/*.css')
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('clean', function() {
+  var del = require('del');
+  return del([
+    'assets/css/*',
+  ]);
+});
+
+gulp.task('sass', ['clean'], function() {
+  var sass = require('gulp-sass');
+  return gulp.src('./src/css/*.scss')
+    .pipe( sass().on('error', sass.logError) )
+    .pipe( gulp.dest('./assets/css/') );
+});
+
+gulp.task('default', ['postcss']);
+
+gulp.task('watch', function() {
+  gulp.watch('./src/css/*.scss', ['default']);
+});
